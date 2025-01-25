9e7ceedde2ef80a967b2c9c72fe4e4b61aaa233f
sophiecooper2 <sophcoop@uw.edu>
2025-01-25 14:27:36 -0800

    env zones

--- Extra/eco-regions-signbase.R
+++ Extra/eco-regions-signbase.R
@@ -13,11 +13,6 @@ signbase_sf$environmental_zone <- terra::extract(env_raster_reproj, signbase_sf)
 signbase_sf <- signbase_sf %>% 
   mutate(environmental_zone = environmental_zone$eea_r_3035_1_km_env)
 
-##Plotting out distribution of signs over environmental zone raster
-
-env_zone_data <- signbase_sf %>% 
-  mutate(environmental_zone_clean = as.factor(environmental_zone))
-  
 
 ggplot(Europe) + 
   geom_spatraster(data = env_raster_reproj) +
@@ -45,8 +40,8 @@ ggplot(signbase_sf) +
 ggplot(Europe) + 
   geom_spatraster(data = env_raster_reproj) +
   geom_sf(data = signbase_sf, 
-          mapping = aes(size = diversity, )) +
-  scale_size_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25)) +
+          mapping = aes(size = diversity), 
+          scale_size_manual(breaks = c(0, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2))) +
   coord_sf(xlim = c(-10,30), 
            ylim = c(35,53), 
            expand = FALSE) +
@@ -54,23 +49,28 @@ ggplot(Europe) +
 
 
 
+
 ## Biogeographical regions of Europe - (https://pmc.ncbi.nlm.nih.gov/articles/PMC7340631/#sec24)
 #3 as determined by the European Environment Agency
 
 bio_geo_raster <- rast("data/bdj-08-e53720-s002.tif")
+bio_geo_raster_df <- 
+  as.data.frame(bio_geo_raster, xy = TRUE) %>% 
+  mutate(`bdj-08-e53720-s002` = as.factor(`bdj-08-e53720-s002`))
 
 res(bio_geo_raster)
 
 signbase_sf$biogeographical_region <- terra::extract(bio_geo_raster, signbase_sf)
 
-
-
 signbase_sf <- signbase_sf %>% 
   mutate(biogeographical_region = biogeographical_region$bdj)
 
 ggplot(Europe) + 
-  geom_spatraster(data = bio_geo_raster) +
-  scale_fill_grass_d(palette = "viridis") +
+  geom_raster(data = bio_geo_raster_df,
+                  aes(x = x,
+                      y = y,
+                      fill = `bdj-08-e53720-s002`)) +
+  #scale_fill_discrete() +
   geom_sf(data = signbase_sf) +
   coord_sf(xlim = c(-10,30), 
            ylim = c(35,53), 
@@ -79,7 +79,8 @@ ggplot(Europe) +
 
 ## elevation data
 library(geodata)
-elev_rast <- elevation_global(res = 0.5, path = "/Users/sophiecooper/Independent Research Study/SignBase/large-files")
+elev_rast <- elevation_global(res = 0.5, path = "/Users/sophiecooper/Independent Research Study/SignBase/large-files") %>% 
+  crop(rast_ext)
 
 
 signbase_sf$elevation <- extract(elev_rast, signbase_sf)
@@ -112,7 +113,8 @@ ggplot(Europe) +
 
 signbase_sf$sign_count <- abundance_data$sign_total
 
-ggplot(signbase_sf) +
+ggplot(signbase_sf %>% 
+         filter(sign_count >= 5)) +
   aes(x = sign_count, y = elevation) +
   geom_point()
 
@@ -125,10 +127,11 @@ ggplot(signbase_sf) +
 # boxplot of elevation by group
 
 ggplot(signbase_sf) +
-  aes(x = group, y = elevation) +
+  aes(x = reorder(group, elevation),
+      y = elevation) +
   geom_boxplot()
 
-##boxplot of elevation by sign
+##boxplot of elevation by signtype
 
 sign_long <- signbase_sf %>% 
   pivot_longer(cols = line:star)
@@ -139,28 +142,116 @@ sign_long <- sign_long %>%
 
 table(sign_long$name)
 
+sign_long <- sign_long %>% 
+  filter(name == "notch" | name == "line"| name =="obline" |
+           name == "dot" | name == "hatching" | name == "cross" |
+           name == "circumnotch" | name == "vulva" | name == "grid" |
+           name == "zigzag")
+
+ggplot(sign_long) +
+  aes(x = name, y = elevation) + 
+  geom_boxplot()
+
 ##climate data
 
+## creation of Koggen Geiger climate zones
+library(climetrics)
+rast_ext <- ext(-10, 30, 35, 53)
 
-clim_files <- list.files(path = "large-files/cclgmbi_2-5m", full.names = TRUE)
+month_avg_min_tmp_files <- list.files(path = "large-files/cclgmtn_2-5m/", full.names = TRUE)
 
-mean_temp_rast <- rast("large-files/cclgmbi_2-5m/cclgmbi1.tif")
+month_avg_min_tmp <- rast(month_avg_min_tmp_files) %>% 
+  crop(rast_ext)
 
-clim_raster<- rast(clim_files)
+month_avy_max_tmp_files <- list.files(path = "large-files/cclgmtx_2-5m/", full.names = TRUE)
 
+month_avg_max_tmp <- rast(month_avy_max_tmp_files) %>% 
+  crop(rast_ext)
 
-signbase_sf$climate <- extract(clim_raster, signbase_sf)
+month_precip_files <- list.files(path = "large-files/cclgmpr_2-5m/", full.names = TRUE)
 
-mean_temp_rast <- rast("large-files/cclgmbi_2-5m/cclgmbi1.tif")
+month_precip <- rast(month_precip_files) %>% 
+  crop(rast_ext)
 
-signbase_sf$mean_temperature <- extract(mean_temp_rast, signbase_sf)
+mean_month_temp_rast <- ((month_avg_min_tmp + month_avg_max_tmp)/2)
+
+koppen_geiger_zones <- kgc(p = month_precip, tmin = month_avg_min_tmp, tmax = month_avg_max_tmp, tmean = mean_month_temp_rast)
+
+koppen_geiger_zones_df <- as.data.frame(koppen_geiger_zones, xy = TRUE)
+
+koppen_geiger_zones_df <- koppen_geiger_zones_df %>% 
+  mutate(mean = as.factor(mean))
 
 ggplot(Europe) + 
-  geom_spatraster(data = mean_temp_rast) +
-  scale_fill_gradient(low = "blue", high = "red") +
+  geom_raster(data = koppen_geiger_zones_df,
+              aes(x = x,
+                  y = y,
+                  fill = mean)) +
   geom_sf(data = signbase_sf) +
-  coord_sf(xlim = c(-10,30), 
+  coord_sf(xlim = c(-10,30),
            ylim = c(35,53), 
            expand = FALSE) +
   facet_wrap(~group)
 
+
+##pca on climate data
+
+
+clim_isodata_df <- as.data.frame(month_avg_min_tmp, xy= TRUE) %>% 
+  st_as_sf(coords = c("x", "y"),
+           remove = FALSE,
+           crs = 4326)
+
+clim_isodata_df$elevation <- extract(elev_rast, clim_isodata_df)
+
+clim_isodata_df$max_monthly_temp <- extract(month_avg_max_tmp, clim_isodata_df)
+
+clim_isodata_df$slope <- extract(slope_data, clim_isodata_df)
+
+
+clim_isodata_df$precipitation <- extract(month_precip, clim_isodata_df)
+
+clim_isodata_df <- clim_isodata_df %>% 
+  unnest(c(max_monthly_temp, precipitation, elevation, slope), names_repair = "universal") %>% 
+  drop_na() %>%
+  st_drop_geometry()
+
+clim_xy <- clim_isodata_df %>% 
+  select(x, y)
+
+clim_xy$rn <- paste((clim_xy$x, clim_xy$y, sep = ","))
+
+clim_isodata_df <- clim_isodata_df %>% 
+  select(-x)
+
+colnames(clim_isodata_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_temp_nov", 
+                               "min_temp_dec", "min_temp_feb", "min_temp_mar", "min_temp_apr",
+                               "min_temp_may", "min_temp_june", "min_temp_jul", "min_temp_aug",
+                               "min_temp_sep", "elevID", "elevation", "maxID", "max_temp_jan", "max_temp_oct", 
+                               "max_temp_nov","max_temp_dec", "max_temp_feb", "max_temp_mar",
+                               "max_temp_apr","max_temp_may","max_temp_june","max_temp_jul", 
+                               "max_temp_aug", "max_temp_sep", "slopeID", "slope", "precipID", "precip_jan",
+                               "precip_oct", "precip_nov", "precip_dec", "precip_feb", "precip_mar",
+                               "precip_apr", "precip_may", "precip_june", "precip_jul", "precip_aug",
+                               "precip_sep")
+
+clim_isodata_df <- clim_isodata_df %>% 
+  select(-maxID, -precipID, -elevID, -slopeID)
+
+rownames(clim_isodata_df) <- clim_xy$rn
+
+library(factoextra)
+library(FactoMineR)
+hire_data <- clim_isodata_df
+
+clim_pca <- PCA(clim_isodata_df, graph = FALSE)
+clim_hcpc <- HCPC(clim_pca, graph = FALSE)
+fviz_cluster(clim_hcpc,
+             repel = TRUE,
+             show.clust.cent = FALSE, 
+             color_labels_by_k = FALSE,
+             ellipse.alpha = 0,
+             ggtheme = theme_bw(),
+             main = "Factor map")
+
+hcpc_dataframe <- clim_hcpc$data.clust
