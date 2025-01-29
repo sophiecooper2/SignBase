19fc341a3f2c46ed7dfe7871f6f86e5fab32b9c1
sophiecooper2 <sophcoop@uw.edu>
2025-01-29 14:59:11 -0800

    climate pca

--- Extra/eco-regions-signbase.R
+++ Extra/eco-regions-signbase.R
@@ -69,31 +69,31 @@ ggplot(Europe) +
 
 ## pca on climate data
 
-clim_pca_df <- as.data.frame(month_avg_min_tmp, xy= TRUE) %>% 
+clim_df <- as.data.frame(month_avg_min_tmp, xy= TRUE) %>% 
   st_as_sf(coords = c("x", "y"),
            remove = FALSE,
            crs = 4326)
 
-clim_pca_df$elevation <- extract(elev_rast, clim_pca_df)
+clim_df$elevation <- extract(elev_rast, clim_df)
 
-clim_pca_df$max_monthly_temp <- extract(month_avg_max_tmp, clim_pca_df)
+clim_df$max_monthly_temp <- extract(month_avg_max_tmp, clim_df)
 
-clim_pca_df$slope <- extract(slope_data, clim_pca_df)
+clim_df$slope <- extract(slope_data, clim_df)
 
-clim_pca_df$precipitation <- extract(month_precip, clim_pca_df)
+clim_df$precipitation <- extract(month_precip, clim_df)
 
-clim_pca_df <- clim_pca_df %>% 
+clim_df <- clim_df %>% 
   st_drop_geometry() %>% 
   drop_na() %>% 
   unnest(c(max_monthly_temp, precipitation, elevation, slope), names_repair = "universal") 
 
-clim_xy <- clim_pca_df %>% 
+clim_xy <- clim_df %>% 
   select(x, y)
 
-clim_pca_df <- clim_pca_df %>% 
+clim_df <- clim_df%>% 
   select(-x)
 
-colnames(clim_pca_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_temp_nov", 
+colnames(clim_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_temp_nov", 
                                "min_temp_dec", "min_temp_feb", "min_temp_mar", "min_temp_apr",
                                "min_temp_may", "min_temp_june", "min_temp_jul", "min_temp_aug",
                                "min_temp_sep", "elevID", "elevation", "maxID", "max_temp_jan", "max_temp_oct", 
@@ -104,22 +104,31 @@ colnames(clim_pca_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_tem
                                "precip_apr", "precip_may", "precip_june", "precip_jul", "precip_aug",
                                "precip_sep")
 
-clim_pca_df <- clim_pca_df %>% 
+clim_df <- clim_df %>% 
   select(-maxID, -precipID, -elevID, -slopeID)
 
-clim_pca_df$x <- clim_xy$x
 
-clim_pca_df$y <- clim_xy$y
+clim_df$x <- clim_xy$x
 
-clim_pca_sf <- clim_pca_df %>% 
-  st_as_sf(coords = c("x", "y"),
-           remove = FALSE,
-           crs = 4326)
+clim_df$y <- clim_xy$y
 
-
-clim_pca_rast <- as_spatraster(clim_pca_df,
+clim_rast <- as_spatraster(clim_df,
                    xycols = 40:41,
                    crs = "EPSG:4326")
 
-library(RStoolbox)
-clim_pca <- terra::princomp(clim_pca_rast)
+library(ENMTools)
+clim_pca <- raster.pca(clim_rast, n = 1)
+
+clim_pca_rast <- clim_pca$rasters
+
+clim_pca_df<- as.data.frame(clim_pca_rast, xy = TRUE) %>% 
+  mutate(PC1 = as.factor(PC1))
+
+ggplot(Europe) +
+  geom_spatraster(data = clim_pca_rast)+
+  scale_fill_grass_c() +
+  geom_sf(data = signbase_sf) +
+  coord_sf(xlim = c(-10,30),
+           ylim = c(35,53), 
+           expand = FALSE)
+
