6000d000f854e38a2806d1fe4d91910834fadd2a
sophiecooper2 <sophcoop@uw.edu>
2025-02-14 10:26:54 -0800

    improved pipeline

--- Extra/eco-regions-signbase.R
+++ Extra/eco-regions-signbase.R
@@ -1,32 +1,14 @@
 ## elevation data
 library(terra)
 library(tidyterra)
-rast_ext <- ext(-10, 30, 35, 53)
 library(geodata)
+
+rast_ext <- ext(-10, 30, 35, 53)
+
 elev_rast <- elevation_global(res = 0.5, path = "/Users/sophiecooper/Independent Research Study/SignBase/large-files") %>% 
   crop(rast_ext)
 
-signbase_sf$elevation <- extract(elev_rast, signbase_sf)
-
-signbase_sf <- signbase_sf %>% 
-  mutate(elevation = elevation$wc2.1_30s_elev)
-
-aspect_data <- terrain(elev_rast, v = "aspect", unit = "degrees", neighbors=8)
 slope_data <- terrain(elev_rast, v = "slope", unit = "degrees", neighbors=8)
-
-signbase_sf$aspect <- extract(aspect_data, signbase_sf)
-signbase_sf$slope <- extract(slope_data, signbase_sf)
-
-signbase_sf <- signbase_sf %>% 
-  mutate(aspect = aspect$aspect)
-
-signbase_sf <- signbase_sf %>% 
-  mutate(slope = slope$slope)
-
-##climate data
-
-## creation of Koggen Geiger climate zones
-library(climetrics)
 
 month_avg_min_tmp_files <- list.files(path = "large-files/cclgmtn_2-5m/", full.names = TRUE)
 
@@ -42,8 +24,6 @@ month_precip_files <- list.files(path = "large-files/cclgmpr_2-5m/", full.names 
 
 month_precip <- rast(month_precip_files) %>% 
   crop(rast_ext)
-
-
 
 clim_df <- as.data.frame(month_avg_min_tmp, xy= TRUE) %>% 
   st_as_sf(coords = c("x", "y"),
@@ -61,15 +41,11 @@ clim_df$precipitation <- extract(month_precip, clim_df)
 clim_df <- clim_df %>% 
   st_drop_geometry() %>% 
   drop_na() %>% 
-  unnest(c(max_monthly_temp, precipitation, elevation, slope), names_repair = "universal") 
+  unnest(c(max_monthly_temp, precipitation, elevation, slope), names_repair = "universal") %>% 
+  mutate(northing = y)
 
-clim_xy <- clim_df %>% 
-  dplyr::select(x, y)
 
-clim_df <- clim_df%>% 
-  dplyr::select(-x)
-
-colnames(clim_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_temp_nov", 
+colnames(clim_df) <-  c("x", "y", "min_temp_jan", "min_temp_oct", "min_temp_nov", 
                                "min_temp_dec", "min_temp_feb", "min_temp_mar", "min_temp_apr",
                                "min_temp_may", "min_temp_june", "min_temp_jul", "min_temp_aug",
                                "min_temp_sep", "elevID", "elevation", "maxID", "max_temp_jan", "max_temp_oct", 
@@ -78,18 +54,14 @@ colnames(clim_df) <-  c("northing", "min_temp_jan", "min_temp_oct", "min_temp_no
                                "max_temp_aug", "max_temp_sep", "slopeID", "slope", "precipID", "precip_jan",
                                "precip_oct", "precip_nov", "precip_dec", "precip_feb", "precip_mar",
                                "precip_apr", "precip_may", "precip_june", "precip_jul", "precip_aug",
-                               "precip_sep")
+                               "precip_sep", "northing")
 
 clim_df <- clim_df %>% 
   dplyr::select(-maxID, -precipID, -elevID, -slopeID)
 
 
-clim_df$x <- clim_xy$x
-
-clim_df$y <- clim_xy$y
-
 clim_rast <- as_spatraster(clim_df,
-                   xycols = 40:41,
+                   xycols = 1:2,
                    crs = "EPSG:4326")
 
 library(ENMTools)
@@ -100,15 +72,7 @@ clim_pca_rast <- clim_pca$rasters
 clim_pca_df<- as.data.frame(clim_pca_rast, xy = TRUE) %>% 
   mutate(PC1 = as.factor(PC1))
 
-
-clim_pca_comp <- clim_pca$pca.object
-
-x <- clim_pca_comp$x %>% 
-  as.data.frame()
-
-x$PC1
-
-
+clim_pca_fill <- clim_pca$pca.object$x
 
 ggplot(Europe) +
   geom_spatraster(data = clim_pca_rast)+
@@ -129,6 +93,11 @@ signbase_sf <- signbase_sf %>%
 ggplot(signbase_sf) +
   aes(x = time_period, y = PCA, fill = group) +
   geom_boxplot()
+
+signbase_sf$elevation <- extract(elev_rast, signbase_sf)
+
+signbase_sf <- signbase_sf %>% 
+  mutate(elevation = elevation$wc2.1_30s_elev)
 
 
 ggplot(signbase_sf) +
