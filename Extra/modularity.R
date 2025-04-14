d1114728d480d2ad525b33084c200e22a2ad23ff
sophiecooper2 <sophcoop@uw.edu>
2025-04-14 10:52:16 -0700

    merge

---
+++ Extra/modularity.R
@@ -0,0 +1,60 @@
+library(robin)
+
+artifact_data <- trans_artifact_data %>% 
+  select_if(~ !is.numeric(.) || sum(.) != 0) %>% 
+  mutate_all(~ as.numeric(. > 0))
+
+jac <- vegdist(artifact_data, "jaccard", binary = TRUE)
+dm <- as.matrix(jac)
+
+disim <- 1 - dm
+disim[disim<0.2] <- 0
+disim_net <- network(disim,
+                     directed=F,
+                     ignore.eval=F,
+                     names.eval='weight')
+
+disim_net %v% 'vertex.names' <- row.names(artifact_data)
+
+modul_matrix <-
+  graph_from_adjacency_matrix(1 - dm, mode = "undirected")
+
+
+mem <- as_membership(parse_number(trans_unique_data$group))
+as_group <- modularity(modul_matrix, membership = mem)
+
+
+library(intergraph)
+
+ig <- asIgraph(disim_net)
+
+as_group <- modularity(ig, membership = mem)
+
+
+g <- graph.adjacency(
+  as.matrix(as.dist(cor(base::t(artifact_data), method="pearson"))),
+  mode="undirected",
+  weighted=TRUE,
+  diag=FALSE
+)
+
+g <- make_graph(edges = row.names(artifact_data), directed = FALSE)
+
+g$weight <- jac
+
+
+g <- graph.adjacency(
+  as.matrix(vegdist(artifact_data, "jaccard", binary = TRUE)),
+  mode="undirected",
+  weighted=TRUE,
+  diag=FALSE
+)
+
+
+g <- graph.adjacency(
+  as.matrix(as.dist(cor(t(artifact_data), method="pearson"))),
+  mode="undirected",
+  weighted=TRUE,
+  diag=FALSE
+)
+as_group <- modularity(g, membership = mem)
