c071e31e8c09dec0ebe14d151e797a3e4a6fe029
Ben Marwick <benmarwick@hotmail.com>
2025-05-27 13:35:13 -0700

    investigating modularity score significance

--- docs/modularity.R
+++ docs/modularity.R
@@ -1,15 +1,18 @@
 
-jac <- vegdist(trans_artifact_data %>% mutate_all(~ as.numeric(. > 0)), "jaccard", binary = TRUE)
+jac <- vegdist(trans_artifact_data %>% 
+                 mutate_all(~ as.numeric(. > 0)), "jaccard", binary = TRUE)
 dm <- as.matrix(jac)
 modul_matrix <-
   graph_from_adjacency_matrix(1 - dm, mode = "undirected")
 mem <- as_membership(parse_number(trans_unique_data$group))
 gt <- modularity(modul_matrix, membership = mem)
 
+
+n <- 10000
 modularity_function<- function(modul_matrix, mem){
   gt <- modularity(modul_matrix, membership = mem)
-  N <- nrow(jac)
-  nsteps <- 10000
+  N <-  nrow(jac)
+  nsteps <- n
   modul_table <- tibble("modularity" = c())
   for (i in 1:nsteps){
     mem_random <- as_membership(c(sample(1:N, N, replace = T)))
@@ -17,13 +20,14 @@ modularity_function<- function(modul_matrix, mem){
     modul_row <- tibble("modularity" = modularity_permute)
     modul_table <- modul_table %>% rbind(modul_row)}
   num <- nrow(modul_table %>% 
-                filter(modularity > gt))
-  p_score <- num/10000
+                filter(modularity >= gt))
+  p_score <- num/n
   return(p_score)
 }
 
+p_value <- modularity_function(modul_matrix, mem)
 
-modularity_function(modul_matrix, mem)
+paste0("p = ", p_value)
 
 
 
