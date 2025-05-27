829c1a2b3795ab9022ef21f27f18b5cb036c4e2b
sophiecooper2 <sophcoop@uw.edu>
2025-05-27 13:38:04 -0700

    stash

--- docs/modularity.R
+++ docs/modularity.R
@@ -6,19 +6,20 @@ modul_matrix <-
 mem <- as_membership(parse_number(trans_unique_data$group))
 gt <- modularity(modul_matrix, membership = mem)
 
+n <- 10000
 modularity_function<- function(modul_matrix, mem){
   gt <- modularity(modul_matrix, membership = mem)
   N <- nrow(jac)
-  nsteps <- 10000
+  nsteps <- n
   modul_table <- tibble("modularity" = c())
   for (i in 1:nsteps){
-    mem_random <- as_membership(c(sample(1:N, N, replace = T)))
+    mem_random <- as_membership(c(sample(1:2, N, replace = T)))
     modularity_permute <- modularity(modul_matrix, membership = mem_random)
     modul_row <- tibble("modularity" = modularity_permute)
     modul_table <- modul_table %>% rbind(modul_row)}
   num <- nrow(modul_table %>% 
-                filter(modularity > gt))
-  p_score <- num/10000
+                filter(modularity >= gt))
+  p_score <- num/n
   return(p_score)
 }
 
